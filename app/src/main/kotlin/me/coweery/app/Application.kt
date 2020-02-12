2f7a6f46894993205c0a25c7822e7705f21449f7
snazina <jsnazina@gmail.com>
2020-02-12 19:02:39 +0300

    added gradle configuration and init spring boot application

---
+++ app/src/main/kotlin/me/coweery/app/Application.kt
@@ -0,0 +1,11 @@
+package me.coweery.app
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+open class Application
+
+fun main(args: Array<String>) {
+    SpringApplication.run(Application::class.java, *args)
+}
